66c7601818b42a542d7177817e72f46326678ce4
mojtabaamiri <mojtaba.a.amiri@outlook.com>
2024-02-23 00:13:41 +0100

    css improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 
-function getRadioPlayerData() {
-  return fetch("https://api.sr.se/api/v2/channels?format=json&size=4").then(
-    (response) => response.json()
+async function getRadioPlayerData() {
+  const response = await fetch(
+    "https://api.sr.se/api/v2/channels?format=json&size=4"
   );
+  return await response.json();
 }
 
 function App() {
@@ -13,9 +14,9 @@ function App() {
     getRadioPlayerData().then((data) => setChannels(data.channels));
   }, []);
   return (
-    <div>
+    <div className=" flex flex-col items-center justify-center w-full h-full gap-6">
       {channels.map((channel, index) => (
-        <div key={index} className="flex flex-row border justify-center">
+        <div key={index} className=" flex flex-row">
           <div>
             <img className="w-32" src={channel.image} alt={channel.name} />
           </div>
