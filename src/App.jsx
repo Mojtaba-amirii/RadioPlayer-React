cfb436b0400f6d914f311c3ce62d47c3a297e712
mojtabaamiri <mojtaba.a.amiri@outlook.com>
2024-02-25 16:13:13 +0100

    layout responsive

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,22 @@ function App() {
     getRadioPlayerData().then((data) => setChannels(data.channels));
   }, []);
   return (
-    <div className=" flex flex-col items-center justify-center w-full h-full gap-6">
+    <div className=" flex flex-col items-center justify-center w-full sm:w-1/2 mx-auto xl:w-1/3 2xl:w-1/5 h-full gap-6 p-4">
       {channels.map((channel, index) => (
         <div key={index} className=" flex flex-row">
-          <div>
-            <img className="w-32" src={channel.image} alt={channel.name} />
+          <div className=" w-1/4">
+            <img
+              className="w-full h-full"
+              src={channel.image}
+              alt={channel.name}
+            />
           </div>
           <div
-            className="flex flex-col text-3xl text-gray-700 gap-4 p-2 "
+            className=" w-3/4 flex flex-col text-3xl text-gray-700 gap-4 p-2 "
             style={{ backgroundColor: `#${channel.color}` }}
           >
             <h1>{channel.name}</h1>
-            <audio controls>
+            <audio controls className="w-auto">
               <source src={channel.liveaudio.url} type="audio/mpeg" />
             </audio>
           </div>
