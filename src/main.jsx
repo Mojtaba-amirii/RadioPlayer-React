c170c5bde833f7d8bcdae7b1dd3295bd1167e19b
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2024-12-23 13:52:34 +0100

    update React and dependencies; refactor main.jsx to use StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 import Header from "./components/Header";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <Header />
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
